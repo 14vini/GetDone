b5a7f3a3c78ab8e5d2935708c3ce7377a9e2080a
Kauã Vini <157426766+14vini@users.noreply.github.com>
2025-04-22 09:48:38 -0300

    Initial Commit

---
+++ GetDone/GetDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetDoneApp.swift
+//  GetDone
+//
+//  Created by Vinicius on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct GetDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
